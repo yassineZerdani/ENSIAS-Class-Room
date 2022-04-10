2b47f609d54a96f3ef8a4a9afbab1a3f1b3c830b
yassineZerdani <yassinezerdani.gd@gmail.com>
2022-04-10 00:09:17 +0000

    First Commit

---
+++ app/src/main/java/com/example/ensiasclassroom/models/Group.java
@@ -0,0 +1,39 @@
+package com.example.ensiasclassroom.models;
+
+import java.util.LinkedList;
+
+public class Group {
+    private String titre;
+    private String description;
+    LinkedList<Etudiant> liste_etudiants;
+
+    public Group(String titre, String description, LinkedList<Etudiant> liste_etudiants ){
+        this.titre= new String(titre);
+        this.description= new String(description);
+        this.liste_etudiants=liste_etudiants;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LinkedList<Etudiant> getListe_etudiants() {
+        return liste_etudiants;
+    }
+
+    public void setListe_etudiants(LinkedList<Etudiant> liste_etudiants) {
+        this.liste_etudiants = liste_etudiants;
+    }
+}
---
+++ app/src/main/java/com/example/ensiasclassroom/models/Professeur.java
@@ -0,0 +1,69 @@
+package com.example.ensiasclassroom.models;
+
+import java.util.LinkedList;
+
+public class Professeur {
+    private String nom;
+    private String prenom;
+    private String tel;
+    //private LinkedList<Groupe> liste_groupe;
+    private String photo;
+    private String departement;
+
+    public Professeur(String nom, String prenom, String tel, String photo, String departement){
+        this.nom= new String(nom);
+        this.prenom= new String(prenom);
+        this.tel= new String(tel);
+        this.photo= new String(photo);
+        this.departement= new String(departement);
+        //this.liste_groupe=liste_groupe;
+
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    /*public LinkedList<Groupe> getListe_groupe() {
+        return liste_groupe;
+    }
+    public void setListe_groupe(LinkedList<Groupe> liste_groupe) {
+        this.liste_groupe = liste_groupe;
+    }
+*/
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getDepartement() {
+        return departement;
+    }
+
+    public void setDepartement(String departement) {
+        this.departement = departement;
+    }
+}
